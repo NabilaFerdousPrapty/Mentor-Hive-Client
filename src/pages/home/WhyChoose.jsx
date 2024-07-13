064046418ca6dd01a6c2286b1c4784bfecd4a3d2
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-07-13 19:25:26 +0600

    customized

--- src/pages/home/WhyChoose.jsx
+++ src/pages/home/WhyChoose.jsx
@@ -2,13 +2,13 @@ import ChooseBanner from "../../assets/Images/tutorfinal.avif";
 
 const WhyChoose = () => {
   return (
-    <section className="md:mt-20 container mx-auto">
+    <div className="md:mt-20 mx-auto">
       <div className="md:flex justify-evenly items-center">
         <div className="flex-1">
-          <img className="w-[500px]" src={ChooseBanner} alt="" />
+          <img className="h-[500px] w-[500px]" src={ChooseBanner} alt="" />
         </div>
         <div className="flex-1 space-y-5">
-          <h1 className="text-3xl text-center font-bold text-primary">
+          <h1 className="text-3xl text-center font-bold text-[#3b478f]">
             Why Choose Us!
           </h1>
           <p className="text-left">
@@ -25,21 +25,21 @@ const WhyChoose = () => {
           </p>
           <div className="flex justify-evenly items-center">
             <h1>
-              <span className="font-bold text-5xl text-primary">3000+</span>{" "}
+              <span className="font-bold text-5xl text-[#3b478f]">3000+</span>{" "}
               <br /> <span className="font-bold">Authentic Tuitions</span>{" "}
             </h1>
             <h1>
-              <span className="font-bold text-5xl text-primary">130+</span>{" "}
+              <span className="font-bold text-5xl text-[#3b478f]">130+</span>{" "}
               <br /> <span className="font-bold">Verified Tutor</span>{" "}
             </h1>
             <h1>
-              <span className="font-bold text-5xl text-primary">10000+</span>{" "}
+              <span className="font-bold text-5xl text-[#3b478f]">10000+</span>{" "}
               <br /> <span className="font-bold">Monthly Users</span>{" "}
             </h1>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
