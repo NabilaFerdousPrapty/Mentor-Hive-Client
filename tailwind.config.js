d214003729546bc67f9c6779640628aded317866
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-07-13 18:46:34 +0600

    Added initial navbar and setup shadcn ui with customize tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,49 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
+  prefix: "",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "15px",
+      screens: {
+        sm: "576px",
+        md: "768px",
+        lg: "992px",
+        xl: "1200px",
+      },
+    },
+    fontFamily: {
+      primary: "var(--font-lato)",
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#3b478f",
+          hover: "#6366F9",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("tailwindcss-animate")],
+};
